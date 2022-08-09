7887c9f1c4b6d264dd7a959cde33adb784249fc7
Oleksandr Barabash <barabashsan4ik@gmail.com>
2022-08-09 16:35:02 +0300

    add burger-menu

--- js/menu.js
+++ js/menu.js
@@ -14,19 +14,34 @@
 // })();
 
 
-(() => {
-  const refs = {
-    menuBtn: document.querySelector('[data-menu-button]'),
-    menu: document.querySelector('[data-menu]'),
-  };
+// (() => {
+//   const refs = {
+//     menuBtn: document.querySelector('[data-menu-button]'),
+//     menu: document.querySelector('[data-menu]'),
+//   };
 
-  refs.menuBtn.addEventListener('click', () => {
-    const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.menuBtn.setAttribute('aria-expanded', !expanded);
+//   refs.menuBtn.addEventListener('click', () => {
+//     const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     refs.menuBtn.setAttribute('aria-expanded', !expanded);
 
-    refs.menuBtn.classList.toggle('is-open');
-    refs.menu.classList.toggle('is-open');
+//     refs.menuBtn.classList.toggle('is-open');
+//     refs.menu.classList.toggle('is-open');
 
-    document.body.classList.toggle('mobile-menu-is-open');
-  });
-})();
+//     document.body.classList.toggle('mobile-menu-is-open');
+//   });
+// })();
+
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById("nav");
+var blue = document.getElementById("blue");
+
+icon.addEventListener("click", function () {
+	icon1.classList.toggle("a");
+	icon2.classList.toggle("c");
+	icon3.classList.toggle("b");
+	nav.classList.toggle("show");
+	blue.classList.toggle("slide");
+});
