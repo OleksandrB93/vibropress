2dbbc95de10a3bc019f365d63d86f21e8578df39
OleksandrB93 <barabashsan4ik@gmail.com>
2023-03-16 23:55:04 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Navbar from 'components/Navbar/Navbar';
 import Home from 'pages/Home/Home';
 import About from 'pages/About/About';
 import Production from 'pages/Production/Production';
+import Production2 from 'pages/Production/Production2/Production2';
 import Service from 'pages/Service/Service';
 
 export const App = () => {
@@ -13,6 +14,12 @@ export const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/production" element={<Production />} />
         <Route path="/vibropress" element={<Production />} />
+        
+        <Route path="/vibropress/model1" element={<Production2 />} />
+        <Route path="/vibropress/model2" element={<Production2 />} />
+        <Route path="/vibropress/model3" element={<Production2 />} />
+        <Route path="/vibropress/model4" element={<Production2 />} />
+
         <Route path="/betonomix" element={<Production />} />
         <Route path="/pressform" element={<Production />} />
         <Route path="/service" element={<Service />} />
